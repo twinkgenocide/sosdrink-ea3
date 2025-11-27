cc4b5851f1560613661d0135b14880fc35b88b8e
twinkgenocide <illusivelove@proton.me>
2025-11-27 00:13:11 -0300

    Implementación DTO Usuario en RestControllers

--- backend/src/main/java/cl/duoc/risani/sosdrink/backend/restcontrollers/UsuarioRestControllers.java
+++ backend/src/main/java/cl/duoc/risani/sosdrink/backend/restcontrollers/UsuarioRestControllers.java
@@ -5,9 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import cl.duoc.risani.sosdrink.backend.dto.UsuarioDTO;
+import cl.duoc.risani.sosdrink.backend.dto.UsuarioMapper;
+import cl.duoc.risani.sosdrink.backend.entities.TipoUsuario;
 import cl.duoc.risani.sosdrink.backend.entities.Usuario;
+import cl.duoc.risani.sosdrink.backend.services.TipoUsuarioServices;
 import cl.duoc.risani.sosdrink.backend.services.UsuarioServices;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*")
@@ -18,22 +25,36 @@ public class UsuarioRestControllers {
 
     @Autowired
     private UsuarioServices usuarioservices;
+    @Autowired
+    private TipoUsuarioServices tipoUsuarioServices;
+    private UsuarioMapper usuarioMapper = new UsuarioMapper();
 
     @PostMapping
-    public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) {
-        Usuario nuevousuario = usuarioservices.crear(usuario);
-        return ResponseEntity.ok(nuevousuario);
+    public ResponseEntity<UsuarioDTO> crearUsuario(@RequestBody UsuarioDTO usuarioDTO) {
+        TipoUsuario tipoUsuario = tipoUsuarioServices.obtenerId(usuarioDTO.getTipoUsuarioId());
+
+        Usuario usuario = usuarioMapper.fromDTO(usuarioDTO);
+        usuario.setClave(usuarioDTO.getClave());
+        usuario.setTipoUsuario(tipoUsuario);
+
+        Usuario usuarioGuardado = usuarioservices.crear(usuario);
+        UsuarioDTO responseDTO = usuarioMapper.toDTO(usuarioGuardado);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
 
     @GetMapping("/{run}")
-    public ResponseEntity<Usuario> obtenerUsuarioPorId(@PathVariable String run) {
+    public ResponseEntity<UsuarioDTO> obtenerUsuarioPorId(@PathVariable String run) {
         Usuario usuario = usuarioservices.obtenerRun(run);
-        return ResponseEntity.ok(usuario);
+        UsuarioDTO usuarioDTO = usuarioMapper.toDTO(usuario);
+        return ResponseEntity.ok(usuarioDTO);
     }
 
     @GetMapping
-    public ResponseEntity<List<Usuario>> listarUsuarios() {
-        List<Usuario> usuarios = usuarioservices.listarTodas();
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
+        List<UsuarioDTO> usuarios = usuarioservices.listarTodas()
+            .stream().map(usuario -> usuarioMapper.toDTO(usuario))
+            .collect(Collectors.toList());
         return ResponseEntity.ok(usuarios);
     }
 
@@ -43,24 +64,20 @@ public class UsuarioRestControllers {
         return ResponseEntity.noContent().build();
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Usuario> intentarIngreso(@RequestBody Usuario usuario) {
-        List<Usuario> allUsuarios = usuarioservices.listarTodas();
-        for (Usuario u : allUsuarios) {
-            if (u.getCorreo().equals(usuario.getCorreo())) {
-                if (u.getClave().equals(usuario.getClave())) {
-                    return ResponseEntity.ok(u);
-                }
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-            }
-        }
-        return ResponseEntity.notFound().build();
-    }
-
     @PutMapping("/{run}")
-    public ResponseEntity<Usuario> actualizarUsuario(@PathVariable String run,
-            @RequestBody Usuario usuarioActualizado) {
-        Usuario usuario = usuarioservices.actualizar(run, usuarioActualizado);
-        return ResponseEntity.ok(usuario);
+    public ResponseEntity<UsuarioDTO> actualizarUsuario(@PathVariable String run, @RequestBody UsuarioDTO usuarioDTO) {
+        Usuario usuario = usuarioservices.obtenerRun(run);
+        TipoUsuario tipoUsuario = tipoUsuarioServices.obtenerId(
+            usuarioDTO.getTipoUsuarioId()
+        );
+
+        usuario.setNombre(usuarioDTO.getNombre());
+        usuario.setApellidos(usuarioDTO.getApellidos());
+        usuario.setClave(usuarioDTO.getCorreo());
+        usuario.setDireccion(usuarioDTO.getDireccion());
+        usuario.setTipoUsuario(tipoUsuario);
+
+        Usuario usuarioGuardado = usuarioservices.actualizar(run, usuario);
+        return ResponseEntity.ok(usuarioMapper.toDTO(usuarioGuardado));
     }
 }
